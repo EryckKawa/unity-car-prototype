e95dd3c03851ac84e344c7d689499314d67d3e5a
Eryck Kawa <okazakiall114@gmail.com>
2024-05-31 17:13:15 -0300

    add new mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private float turnSpeed;
 	private float horizontalInput;
 	private float verticalInput;
+	[SerializeField] private Camera mainCamera;
+	[SerializeField] private Camera hoodCamera;
+	[SerializeField] private KeyCode switchKey;
 	// Start is called before the first frame update
 	private void Start()
 	{
@@ -22,5 +25,13 @@ public class PlayerController : MonoBehaviour
 		//Move the vehicle forward
 		transform.Translate(movSpeed * Time.deltaTime * verticalInput * Vector3.forward);
 		transform.Rotate(horizontalInput * Time.deltaTime * turnSpeed * Vector3.up);
+
+		if (Input.GetKeyDown(switchKey))
+		{
+			// Alterna o estado habilitado/ativado da câmera principal
+			// Se a câmera principal estiver ativada, ela será desativada e vice-versa
+			mainCamera.enabled = !mainCamera.enabled;
+			hoodCamera.enabled = !hoodCamera.enabled;
+		}
 	}
 }
--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PropellerSpin : MonoBehaviour
+public class VehicleController : MonoBehaviour
 {
-	[SerializeField] private float spinSpeed;
+	[SerializeField] private float moveSpeed;
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		transform.Rotate(spinSpeed * Time.deltaTime * Vector3.forward);
+		transform.Translate(moveSpeed * Time.deltaTime * Vector3.forward);
 	}
 }
